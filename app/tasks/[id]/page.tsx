ac3f4be2b1e9c4c84ee67315e41779f05e584075
int-sonu <sonusebastian2025@mca.ajce.in>
2025-12-26 17:23:26 +0530

    second commit

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,29 +1,34 @@
 import { Task } from "@/Type/tasks";
 
-export default async function TaskDetail({ params }: { params: { id: string } }) {
-  const baseurl = process.env.BASE_URL || "http://localhost:3000"
-  const { id } = await params;
-  console.log("ID from URL:", id);
-  const res = await fetch(`${baseurl}/api/tasks/${id}`, {
-    method: "GET",
+export default async function TaskDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+
+  const res = await fetch(`/api/tasks/${id}`, {
     cache: "no-store",
   });
 
-  const task: Task = await res.json();
-  console.log("API response:", task);
-
-  if (!task) {
+  if (!res.ok) {
     return <div className="p-6">Task Not Found</div>;
   }
 
+  const task: Task = await res.json();
+
   return (
-
     <div className="p-6">
-
       <h1 className="text-2xl font-bold">Task Details</h1>
+
       <div className="mt-4 border p-4 rounded">
-        <p><strong>Task:</strong> {task?.title}</p>
-        <p><strong>Status:</strong> {task.completed ? "Completed" : "Pending"}</p>
+        <p>
+          <strong>Task:</strong> {task.title}
+        </p>
+        <p>
+          <strong>Status:</strong>{" "}
+          {task.completed ? "Completed" : "Pending"}
+        </p>
       </div>
     </div>
   );

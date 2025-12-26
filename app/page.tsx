9c2f1046a04d3f76cc456eaa90fc78dba3dd3c4b
int-sonu <sonusebastian2025@mca.ajce.in>
2025-12-26 20:28:32 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,44 @@
 import Taskcard from "@/components/TaskCard";
-import Image from "next/image";
 
+export const metadata = {
+  title: "Home | Task Tracker",
+  description: "View and manage tasks",
+};
 
-export const metadata={
-  title:"Home | Task Tracker",
-  description:"View and manage tasks"
-}
 async function getTask() {
-    const baseurl=process.env.BASE_URL ||"http://localhost:3000"
-    const res=await fetch(`${baseurl}/api/tasks`,{cache:'no-store'})
-    return res.json()
+  const res = await fetch("/api/tasks", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch tasks");
   }
 
-export default async function Home(){
-  const tasks=await getTask()
-  return(
+  return res.json();
+}
+
+export default async function Home() {
+  const tasks = await getTask();
+
+  return (
     <div className="p-8 max-w-3xl mx-auto">
-  <div className="flex items-center justify-between mb-8">
-    <h1 className="text-4xl font-bold bg-linear-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent tracking-tight">
-      Task Tracker
-    </h1>
-  </div>
-  <ul className="space-y-4">
-    {tasks.length === 0 && (
-      <p className="mt-10 text-gray-500 text-center text-lg">
-        No tasks found.
-      </p>
-    )}
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="text-4xl font-bold bg-linear-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent tracking-tight">
+          Task Tracker
+        </h1>
+      </div>
 
-    {tasks.map((task: any) => (
-      <Taskcard key={task._id} task={task} />
-    ))}
-  </ul>
-</div>
+      <ul className="space-y-4">
+        {tasks.length === 0 && (
+          <p className="mt-10 text-gray-500 text-center text-lg">
+            No tasks found.
+          </p>
+        )}
 
+        {tasks.map((task: any) => (
+          <Taskcard key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
   );
 }

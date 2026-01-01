d4c8d016d280209c9aecc537e1ebafc98ef527b9
int-sonu <sonusebastian2025@mca.ajce.in>
2026-01-01 16:58:09 +0530

    route.ts

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,64 +1,30 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
-import Task from "@/models/task";
-import mongoose from "mongoose";
-
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export async function PUT(req: Request, { params }: Params) {
-  await connectDB();
-
-  const { id } = params; 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
-  }
-
-  const { completed } = await req.json();
-
-  const updated = await Task.findByIdAndUpdate(
-    id,
-    { completed },
-    { new: true }
-  );
-
-  if (!updated) {
-    return NextResponse.json({ error: "Task not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(updated);
+import task from "@/models/task";
+export async function PUT(req: Request, context: any) {
+    await connectDB()
+    const { id } = await context.params
+    const { completed } = await req.json()
+    const update = await task.findByIdAndUpdate(
+        id,
+        { completed },
+        { new: true }
+    )
+    if (!update) return NextResponse.json({ error: "Task not found" }, { status: 404 })
+    return NextResponse.json(update)
 }
+export async function GET(req: Request, { params }: any) {
+    await connectDB();
+    const { id } = await params;
+    console.log("url:", id);
 
-export async function GET(_: Request, { params }: Params) {
-  await connectDB();
-
-  const { id } = params; 
-
-  const task = await Task.findById(id);
-
-  if (!task) {
-    return NextResponse.json({ error: "Task not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(task);
+    const task_new = await task.findById(id);
+    return NextResponse.json(task_new, { status: 200 });
 }
-
-export async function DELETE(_: Request, { params }: Params) {
-  await connectDB();
-
-  const { id } = params; 
-
-  const deleted = await Task.findByIdAndDelete(id);
-
-  if (!deleted) {
-    return NextResponse.json({ error: "Task not found" }, { status: 404 });
-  }
-
-  return NextResponse.json(
-    { message: "Successfully deleted" },
-    { status: 200 }
-  );
+export async function DELETE(req: Request, { params }: any) {
+    await connectDB()
+    const { id } = await params
+    const deletetask = await task.findByIdAndDelete(id)
+    if (!deletetask) return NextResponse.json({ error: "Task not found" }, { status: 404 })
+    return NextResponse.json({ message: "success fully deleted" }, { status: 200 })
 }

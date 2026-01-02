a01d32eb29439dacf62ab662a66f1f89a7f0782c
int-sonu <sonusebastian2025@mca.ajce.in>
2026-01-02 14:48:17 +0530

    newone

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,24 @@
 import Taskcard from "@/components/TaskCard";
+import { connectDB } from "@/lib/db";
+import TaskModel from "@/models/task";
 
 async function getTasks() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/tasks`,
-    {
-      cache: "no-store", 
-    }
-  );
+  await connectDB();
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch tasks");
-  }
+  const tasks = await TaskModel.find().lean();
 
-  return res.json();
+  return tasks.map((task) => ({
+    _id: task._id.toString(),  
+    title: task.title,
+    completed: task.completed,
+  }));
 }
 
 export default async function Home() {
   const tasks = await getTasks();
 
   return (
-    <main
-      className="min-h-[calc(100vh-72px)] pt-20 px-4
+    <main className="min-h-[calc(100vh-72px)] pt-20 px-4
       bg-gradient-to-br from-blue-500 via-indigo-500 to-violet-600"
     >
       <ul className="max-w-2xl mx-auto space-y-4">
@@ -30,7 +28,7 @@ export default async function Home() {
           </p>
         )}
 
-        {tasks.map((task: any) => (
+        {tasks.map((task) => (
           <Taskcard key={task._id} task={task} />
         ))}
       </ul>

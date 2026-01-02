a74a899c397a4e32d85c6479b102f63fade0c279
int-sonu <sonusebastian2025@mca.ajce.in>
2026-01-02 12:05:58 +0530

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
-// import Taskcard from "@/components/TaskCard";
-// import { connectDB } from "@/lib/db";
-// import Task from "@/models/task";
+import Taskcard from "@/components/TaskCard";
+import { connectDB } from "@/lib/db";
+import Task from "@/models/task";
 
-// export const metadata = {
-//   title: "Home | Task Tracker",
-//   description: "View and manage tasks",
-// };
+export const metadata = {
+  title: "Home | Task Tracker",
+  description: "View and manage tasks",
+};
 
-// async function getTask() {
-//   await connectDB();
-//   return Task.find().lean();
-// }
+async function getTask() {
+  await connectDB();
+  return Task.find().lean();
+}
 
-// export default async function Home() {
-//   const tasks = await getTask();
+export default async function Home() {
+  const tasks = await getTask();
 
-//   return (
-//     <div className="p-8 max-w-3xl mx-auto">
-//       <ul className="space-y-4">
-//         {tasks.length === 0 && <p>No tasks found</p>}
-//         {tasks.map((task: any) => (
-//           <Taskcard key={task._id} task={task} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+  return (
+    <div className="p-8 max-w-3xl mx-auto">
+      <ul className="space-y-4">
+        {tasks.length === 0 && <p>No tasks found</p>}
+        {tasks.map((task: any) => (
+          <Taskcard key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+}
